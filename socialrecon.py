8e3a402606b6412525cb81b06656f4223f2ab475
kobra <100838144+whitekobra69@users.noreply.github.com>
2023-02-24 23:10:20 +0530

    Update socialrecon.py

--- socialrecon.py
+++ socialrecon.py
@@ -2,6 +2,7 @@ import time
 import sys
 from url import urlinfo
 from pdfanalysis import pdfinfo
+from imagerecon import Recon
 from iplocator import iplocate
 from TraceIP import read_multiple_ip
 from webscrap import Links
